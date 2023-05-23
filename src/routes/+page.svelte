9db67ddc8d2c0a28f09bb16682c3d8e1e17459b4
olivervaz <olgashambir@gmail.com>
2023-05-23 22:32:18 +0200

    word cloud added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import logo from '@lib/assets/logo.png';
+    import logo from '@lib/images/logo.svg';
     import Section from '@components/Section/Section.svelte';
     import Grid from '@components/Grid/Grid.svelte';
     import Link from '@components/Link/Link.svelte';
@@ -11,22 +11,23 @@
     <meta name="description" content="Home"/>
 </svelte:head>
 <main>
-    <Grid axis="x">
-        <Grid axis="y">
+    <Grid axis="x" class="title">
+        <div class="title">
             <h1 class="mb0">Olha Shambir</h1>
             <h3>Frontend Developer</h3>
-            <p>
-                Hi, I'm Olha Shambir, an experienced frontend developer with a passion for crafting exceptional web
-                experiences.
-                <br/>
-                Proficient in JavaScript and TypeScript, with expertise in React Native.
-                Skilled in creating responsive and user-friendly interfaces.
-                (
-                <Link href="/about">More details</Link>
-                )
-            </p>
-        </Grid>
-        <img src={logo} alt="logo"/>
+        </div>
+<!--        <div class="image">-->
+<!--            <img src={logo} alt="logo"/>-->
+<!--        </div>-->
+    </Grid>
+    <Grid axis="y">
+        <p>
+            Hi, I'm Olha Shambir, an experienced frontend developer with a passion for crafting exceptional web experiences.
+            <br/>
+            Proficient in JavaScript and TypeScript, with expertise in React Native.
+            Skilled in creating responsive and user-friendly interfaces.
+            (<Link href="/about">More details</Link>)
+        </p>
     </Grid>
     <Section title={"Latest posts"}>
         <BlogWidget/>
@@ -36,16 +37,7 @@
     </Section>
 </main>
 <style>
-    h3 {
-        margin-top: 4px;
-    }
-
-    main {
-        padding: 32px 0;
-    }
-
-    img {
-        max-width: 225px;
-        align-self: flex-start;
+    .title {
+        margin-bottom: 1.5em;
     }
 </style>
